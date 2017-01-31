50665e5f7484d86f023d28ecd8e39b28912f6661
sourcered <=>
2017-01-31 16:34:12 -0200

    Implementing querys

--- console-debug.cpp
+++ console-debug.cpp
@@ -3,6 +3,6 @@
 int main()
 {
     std::unique_ptr<nobuk::Core> core (new nobuk::Core);
-
+    
     return 0;
 }
--- include/database/querys.h
+++ include/database/querys.h
@@ -4,8 +4,23 @@
 #include "../preprocessor.h"
 
 #if 1
-const std::string SQL_CREATE_DATABASE = "";
-const std::string SQL_CREATE_TABLE = "";
+
+#ifndef PRE_LOG
+#define PRE_LOG
+const std::string DATABASE_NAME = "dbnobuk";
+const std::string TABLE_NAME = "inventory";
+const std::string PASSWORD = "friday19";
+#endif
+
+const std::string SQL_CREATE_DATABASE = "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME;
+const std::string SQL_CREATE_TABLE =    "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(60) NOT NULL DEFAULT \'No name\', code INT NOT NULL UNIQUE, quantity DEC(7,3) NOT NULL DEFAULT 0, price DEC(6,2) NOT NULL DEFAULT 0, total DEC(8,2) NOT NULL DEFAULT 0)";
+const std::string SQL_UPDATE_NAME =     "UPDATE " + TABLE_NAME + " SET name=? WHERE id=?";
+const std::string SQL_UPDATE_CODE =     "UPDATE " + TABLE_NAME + " SET code=? WHERE id=?";
+const std::string SQL_UPDATE_QUANTITY = "UPDATE " + TABLE_NAME + " SET quantity=? WHERE id=?";
+const std::string SQL_UPDATE_PRICE =    "UPDATE " + TABLE_NAME + " SET price=? WHERE id=?";
+const std::string SQL_DROP_DATABASE =   "DROP DATABASE " + DATABASE_NAME;
+const std::string SQL_DROP_TABLE =      "DROP TABLE " + TABLE_NAME;
+const std::string SQL_UPDATE_IDS =      "";
 
 #else
 constexp std::string SQL_CREATE_DATABASE = "";
